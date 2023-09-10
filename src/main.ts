e4436b6d4ee928f540b87f0b7941b3a4e783761c
Chris Krycho <hello@chriskrycho.com>
2023-09-10 17:17:31 -0600

    Implement basic support for invoking dprint via stdin
    - Introduce an extension class to handle extension state: creating it
      with a configured path, cleaning it up at deactivation, etc.
    
    - Define the basic workflow for invoking dprint via stdin using pipes.
      While this is not particularly *elegant*, it is extremely *fast*,
      because dprint itself is very fast, and it has the advantage of not
      requiring the use (yet) of `@dprint/formatter`. Good MVP approach.
      Might even work for formatting ranges if I am clever later.
    
    - Wire up the commands to match the definitions from the extension
      definition. It would be great if I could make this type safe. :sigh:
      And maybe I can, by using something like Zod or whatnot and producing
      a typed schema for the extension config or something, and then *only*
      doing that in dev, or... something. I don't know. Strings everywhere.
      :sigh:

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,366 @@
-// Build it!
+// Running via stdin: `dprint fmt --stdin <file-path/file-name/extension>`. Here
+// I am starting out by using the actual `<file-path>` because that respects
+// config, which means there is no need to *manage* config. However, doing it
+// that way means just completely reformatting the whole editor and losing track
+// of where the cursors is as a result, and it also does not support formatting
+// only *parts* of the document.
+
+import Maybe, { just, nothing } from 'true-myth/maybe';
+import Result, { match } from 'true-myth/result';
+import { toOkOrErr } from 'true-myth/toolbelt';
+
+let { fs, config, notifications, workspace } = nova;
+
+let extension: DprintExtension | undefined;
+let topLevelDisposables = new Set<Disposable>();
+
+export function activate() {
+   info('activating dprint extension');
+   tryToInstall();
+
+   topLevelDisposables.add(workspace.onDidChangePath(_ => {
+      extension?.cleanup();
+      tryToInstall();
+   }));
+}
+
+export function deactivate() {
+   extension?.cleanup();
+   topLevelDisposables.forEach(d => d.dispose());
+   topLevelDisposables.clear();
+}
+
+function tryToInstall() {
+   info('installing dprint extension');
+
+   DprintExtension.for(workspace).match({
+      Ok: (instance) => {
+         info('successfully installed extension');
+         extension = instance;
+      },
+      Err: (reason) => {
+         error('failed to install dprint extension');
+         addNotification({
+            id: 'dprint.bad-workspace',
+            title: 'workspace configuration',
+            body: reason,
+         });
+      },
+   });
+}
+
+function addNotification({ id, title, body }: {
+   id: string;
+   title: string;
+   body: string;
+}) {
+   let request = new NotificationRequest(id);
+   request.title = l(title);
+   request.body = body;
+   request.actions = [l('Ok')];
+
+   notifications.add(request).catch((error: unknown) => {
+      console.error(`Failed to add notification: ${error}`);
+   });
+}
+
+class DprintExtension {
+   readonly path: string;
+
+   #disposables: {
+      format: Maybe<Disposable>;
+      formatSelection: Maybe<Disposable>;
+      formatOnSave: Maybe<Disposable>;
+      saveWithoutFormatting: Maybe<Disposable>;
+      didChange: Maybe<Disposable>;
+   };
+
+   #ignoredEditors: Set<TextEditor> = new Set();
+
+   static for(workspace: Workspace): Result<DprintExtension, string> {
+      return pathForDprint(workspace).map((path) => new this(path));
+   }
+
+   private constructor(path: string) {
+      info(`constructing dprint extension for '${path}'`);
+      this.path = path;
+
+      this.#disposables = {
+         format: just(nova.commands.register(
+            'dprint.format',
+            (editor: TextEditor) => {
+               info('running `format` from format command');
+               format(editor);
+            },
+         )),
+
+         // This may not be doable in a good way right now: dprint has top-level
+         // caching itself, but Node does *not*. `@dprint/formatter` is the path
+         // if I *do* try to implement it, though.
+         formatSelection: just(nova.commands.register(
+            'dprint.format-selection',
+            (editor: TextEditor) => {
+               info('running `format` from `formatSelection` command');
+               let ranges = editor.selectedRanges;
+               for (let range of ranges) {
+                  // TODO: call format?
+                  // To make this work, I think I would need to cache it
+                  // *myself*. Gross.
+               }
+            },
+         )),
+
+         formatOnSave: Maybe.of(workspace.activeTextEditor).map(activeEditor =>
+            activeEditor.onWillSave((editor) => {
+               info('running `format` from `formatOnSave`');
+               if (!this.#ignoredEditors.has(editor)) return format(editor);
+            })
+         ),
+
+         saveWithoutFormatting: just(nova.commands.register(
+            'dprint.save-without-formatting',
+            (editor: TextEditor) => {
+               this.#ignoredEditors.add(editor);
+               safely(editor.save(), (reason) => JSON.stringify(reason)).then(match({
+                  Ok: (_) => {
+                     this.#ignoredEditors.delete(editor);
+                  },
+                  Err: (reason) => {
+                     addNotification({
+                        id: 'dprint.error.save-without-formatting',
+                        title: 'dprint error',
+                        body:
+                           `Failed to save without executing dprint formatter. ${reason}`,
+                     });
+                  },
+               }));
+            },
+         )),
+
+         didChange: just(config.onDidChange('dprint.general.path', () => {
+            todo('dprint path config change');
+         })),
+      };
+   }
+
+   cleanup() {
+      Object.values(this.#disposables);
+      for (let disposable of Object.values(this.#disposables)) {
+         if (disposable.isJust) disposable.value.dispose();
+      }
+   }
+}
+
+function format(editor: TextEditor): Promise<void> {
+   // TODO: handle remote documents?
+   // TODO: handle unsaved documents using `unsaved://` URI scheme?
+   if (editor.document.path) {
+      info(`invoking dprint on ${editor.document.path}`);
+      return invokeDprintOn(editor.document).then(match({
+         Ok(formatted) {
+            info('successfully returned from dprint');
+            editor.edit((edit) => {
+               info('editing with formatted code');
+               let range = new Range(0, editor.document.length);
+               edit.replace(range, formatted);
+            });
+         },
+         Err(reason) {
+            error('failed to dprint', reason);
+            addNotification({
+               id: 'dprint.error.format',
+               title: 'Could not format',
+               body: reason,
+            });
+         },
+      }));
+   } else {
+      return Promise.resolve();
+   }
+}
+
+// NOTE: the workspace might not have a path, I think., as in the case where it
+// is a non-rooted Nova project.
+
+function pathForDprint(workspace: Workspace): Result<string, string> {
+   let configPath = getConfig(workspace.config, 'dprint.general.path', 'string')
+      .or(getConfig(nova.config, 'dprint.general.path', 'string'));
+
+   info(getConfig(nova.config, 'dprint.general.path', 'string'));
+
+   // When the user has specified a config path, that will override any
+   // per-project or global installation.
+   if (configPath.isJust) {
+      return checkPath(configPath.value);
+   }
+
+   // Otherwise, check for a local installation or a global installation to use.
+   let projectPath = Maybe.of(workspace.path);
+
+   let fromProjectInstall = todo('get path from project, if any') as Maybe<string>; // use projectPath
+   let fromGlobalInstall = todo('get path from global, if any') as Maybe<string>;
+
+   todo('get path for dprint');
+}
+
+function requireString(value: unknown): Result<string, string> {
+   return typeof value === 'string'
+      ? Result.ok(value)
+      : Result.err(`${JSON.stringify(value)} is not a string`);
+}
+
+function checkPath(path: string): Result<string, string> {
+   // Technically subject to TOCTOU bugs, and slightly slower by way of doing
+   // two stats, but in this case we don't actually care; the point is to (try
+   // to) give better error messages.
+   if (!fs.access(path, fs.F_OK)) return Result.err(`Path '${path}' does not exist.`);
+   if (!fs.access(path, fs.X_OK)) return Result.err(`Path '${path}' is not executable`);
+   return Result.ok(path);
+}
+
+function asyncErr<T, E>(reason: E): Promise<Result<T, E>> {
+   return Promise.resolve(Result.err(reason));
+}
+
+function invokeDprintOn(document: TextDocument): Promise<Result<string, string>> {
+   if (!extension) {
+      return asyncErr('Tried to invoke dprint without an installed extension');
+   }
+
+   if (!document.path) {
+      todo('implement support for formatting paths based on type when missing path');
+   }
+
+   let process = new Process(extension.path, {
+      args: ['fmt', '--stdin', document.path],
+      stdio: 'pipe',
+      // cwd: defaults (correctly) to project directory; configurable?
+   });
+
+   let { resolve, promise } = defer<Result<string, string>>();
+
+   let outBuffer = new Array<string>();
+   let errBuffer = new Array<string>();
+   process.onStdout((result) => outBuffer.push(result));
+   process.onStderr((reason) => errBuffer.push(reason));
+   process.start();
+
+   let stdin = process.stdin?.getWriter();
+   if (!stdin) return Promise.resolve(Result.err('could not get a handle to stdin'));
+
+   stdin.write(document.getTextInRange(new Range(0, document.length)));
+   stdin.close();
+
+   process.onDidExit((status) => {
+      if (status === 0) {
+         if (errBuffer.length > 0) {
+            resolve(
+               Result.err(
+                  'process exited successfully but there were errors; not applying formatting'
+                     + errBuffer.join(''),
+               ),
+            );
+         } else {
+            resolve(Result.ok(outBuffer.join('')));
+         }
+      } else {
+         let base = `process exited with a failure (${status})`;
+         let fromStderr = errBuffer.length > 0 ? '\n' + errBuffer.join('') : '';
+         let message = `${base}${fromStderr}`;
+         resolve(Result.err(message));
+      }
+   });
+
+   return promise;
+}
+
+interface Deferred<T> {
+   promise: Promise<T>;
+   resolve: (value: T) => void;
+   reject: (reason: unknown) => void;
+}
+
+function defer<T>(): Readonly<Deferred<T>> {
+   // SAFETY: we will immediately fill in all the values here.
+   let deferred: Deferred<T> = {} as Deferred<T>;
+   deferred.promise = new Promise((resolve, reject) => {
+      deferred.resolve = resolve;
+      deferred.reject = reject;
+   });
+   return deferred;
+}
+
+function safely<T, E>(
+   promise: Promise<T>,
+   onReject: (reason: unknown) => E,
+): Promise<Result<T, E>> {
+   return promise.then(
+      (value) => Result.ok(value),
+      (reason) => Result.err(onReject(reason)),
+   );
+}
+
+function todo(details: string): never {
+   throw (`TODO: ${details}`);
+}
+
+function l(s: string): string {
+   return nova.localize(s);
+}
+
+// This would be a good thing to do in toolbelt, maybe?
+declare module 'true-myth/maybe' {
+   interface MaybeImpl<T> {
+      toOkOrErr<E>(this: Maybe<T>, err: E): Result<T, E>;
+   }
+}
+
+Maybe.prototype.toOkOrErr = function<E>(err: E) {
+   return toOkOrErr(err, this);
+};
+
+function info(...values: unknown[]): void {
+   if (canLog()) console.info(...values);
+}
+
+function log(...values: unknown[]): void {
+   console.log(...values);
+}
+
+function error(...values: unknown[]): void {
+   console.error(...values);
+}
+
+function canLog(): boolean {
+   let debugEnabled = getConfig(nova.config, 'dprint.general.debug-logging', 'boolean')
+      .unwrapOr(false);
+   return nova.inDevMode() || debugEnabled;
+}
+
+function getConfig(config: Configuration, key: string, type: 'string'): Maybe<string>;
+function getConfig(config: Configuration, key: string, type: 'boolean'): Maybe<boolean>;
+function getConfig(config: Configuration, key: string, type: 'number'): Maybe<number>;
+function getConfig(config: Configuration, key: string, type: 'array'): Maybe<string[]>;
+function getConfig(
+   config: Configuration,
+   key: string,
+   type: 'string' | 'boolean' | 'number' | 'array',
+): Maybe<string | boolean | number | string[]> {
+   return Maybe.of(config.get(key))
+      .andThen((value) => {
+         switch (type) {
+            case 'array':
+               return Array.isArray(value) ? just(value) : nothing();
+            case 'boolean':
+            case 'number':
+            case 'string':
+               return typeof value === type ? just(value) : nothing();
+            default:
+               unreachable(type);
+         }
+      });
+}
+
+function unreachable(x: never): never {
+   throw new Error(`Should be unreachable with value ${x}`);
+}
